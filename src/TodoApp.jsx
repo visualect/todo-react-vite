8c8c928fdb0986339e133e6bfffdbda625c3d5e1
Yan <visualection@gmail.com>
2023-02-27 20:27:55 +0200

    Added possibility to mark item as done

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,4 +1,5 @@
 import { useRef, useState } from 'react';
+import { Toaster, toast } from 'sonner';
 import TodoItem from './TodoItem';
 
 export default function TodoApp() {
@@ -16,7 +17,6 @@ export default function TodoApp() {
     }
 
     setTodos([...todos, newItem]);
-    console.log(todos);
     inputRef.current.value = '';
   }
 
@@ -24,14 +24,34 @@ export default function TodoApp() {
     setTodos(todos.filter(item => item.id !== id));
   }
 
+  function completeItem(id) {
+    setTodos(todos.map(item => {
+      if (item.id === id) {
+        item.done = !item.done
+        return item;
+      }
+      return item;
+    }));
+  }
+
   return (
     <div className='container'>
+      <Toaster />
       <form className="form" onSubmit={handleSubmit}>
         <input className="form__input" type="text" placeholder="Do" ref={inputRef} />
         <button className="btn btn-form">Add</button>
       </form>
       <ul className='list'>
-        {todos.map(item => <TodoItem text={item.text} key={item.id} deleteItem={() => deleteItem(item.id)} />)}
+        {todos.map((item, idx) => <TodoItem
+          text={item.text}
+          key={idx}
+          deleteItem={() => {
+            deleteItem(item.id)
+            toast('Item successfully deleted');
+          }}
+          completeItem={() => completeItem(item.id)}
+          done={item.done}
+        />)}
       </ul>
     </div>
   )
